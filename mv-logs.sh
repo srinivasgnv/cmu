c5f5aa42eab82c0f0c069ce48a524afb8711b4e7
srinivas-gannavarapu <srinivas.gannavarapu@in.ibm.com>
2017-09-23 09:08:48 +0530

    added move script to clean up rpi space

---
+++ mv-logs.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+ts=`date +"%d-%m-%y-%T"`
+SRC="/home/pi/"
+DES="/home/pi/exDisk"
+
+if [ -f "$SRC/pi-temp-volts.log" ]
+then
+	mv $SRC/pi-temp-volts.log $DES/pi-temp-volts-$ts
+fi
+
+if [ -f "$SRC/admin-audit.log" ]
+then
+	mv $SRC/admin-audit.log $DES/admin-audit-$ts
+fi
+
+if [ -f "$SRC/internet-download-upload-speed.log" ]
+then
+	mv $SRC/internet-download-upload-speed.log $DES/internet-download-upload-speed-$ts
+fi
+
+if [ -f "$SRC/rack-temp-humidity.log" ]
+then
+	mv $SRC/rack-temp-humidity.log $DES/rack-temp-humidity-$ts
+fi
+
+
+if [ -f "$SRC/nvr-router-modem-DOWN.log" ]
+then
+	mv $SRC/nvr-router-modem-DOWN.log $DES/nvr-router-modem-DOWN-$ts
+fi
+
+if [ -f "$SRC/internet-up-down.log" ]
+then
+	mv $SRC/internet-up-down.log $DES/internet-up-down-$ts
+fi
